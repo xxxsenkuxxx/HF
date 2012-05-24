f8bb6ff691f1f085692ca87946359a3219ec0c62
senku7 <senku7@gmail.com>
2012-05-24 19:29:56 +0900

    init

---
+++ Herb/src/abc/d/HerbActivity.java
@@ -0,0 +1,13 @@
+package abc.d;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HerbActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
